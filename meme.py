04553ac9a71e7323ba37f8e8d2df51880b01ec10
leon2401119 <leon2401119.cs04@nctu.edu.tw>
2021-08-03 12:45:22 +0800

    framework construction 80% done, surrogate model soon

--- meme.py
+++ meme.py
@@ -4,11 +4,15 @@ import random
 import subprocess
 import glob
 from multiprocessing import Pool
+import copy
+
 
 CBENCH_PATH = 'cBench_V1.1'
 TARGET = 'automotive_bitcount'
+TMP_DIR = 'tmpfiles'
 FLAGS = None
 WORKERS = 4
+O0_SIZE = None
 
 class GA:
     def __init__(self,ell,pop_size=200, p_xo=0.8, p_elite=0.1):
@@ -18,12 +22,91 @@ class GA:
         self.p_elite = p_elite
 
     def init(self):
-        self.pop = [[random.randint(0,1) for _ in range(ell)] for _ in range(self.pop_size)]
-        self.fitness = [self.eval(i) for i in self.pop]
+        # chromosome is formatted as [[genes],fitness], fitness is None when
+        # the chromosomenot is not evaulated yet
+        self.pop = [[[random.randint(0,1) for _ in range(self.ell)],None] for _ in range(self.pop_size)]
+        self.__eval_pop(self.pop)
 
-    def run(gen):
+    def __xo(self,pop):
+        # roulette-wheel
+        # FIXME : what about negative value??
+        # prepare the base for rw selection
+        # chromosomes that cause compile error will not be preserved at all
+        adjusted_pop = [ch for ch in pop if ch[1] is not None]
+        if adjusted_pop[-1][1] < 0:
+            for ch in adjusted_pop:
+                ch[1] -= adjusted_pop[-1][1] # pop is sorted by fitness already
+        
+        total_fitness = 0
+        for ch in adjusted_pop:
+            total_fitness += ch[1]
+
+        new_pop = []
+
+        while len(new_pop) < len(pop):
+            tmp1 = random.randint(1,total_fitness)
+            tmp2 = random.randint(1,total_fitness)
+            accum,i = 0,-1
+            while accum < tmp1:
+                i += 1
+                accum += adjusted_pop[i][1]
+            parent_a = copy.deepcopy(adjusted_pop[i])
+            accum,i = 0,-1
+            while accum < tmp2:
+                i += 1
+                accum += adjusted_pop[i][1]
+            parent_b = copy.deepcopy(adjusted_pop[i])
+
+            if random.random() < self.p_xo: # do crossover (one-point for now)
+                site = random.randint(1,len(parent_a[0])-2)
+                parent_a[0][site:], parent_b[0][:site] = parent_b[0][site:], parent_a[0][:site]
+                parent_a[1], parent_b[1] = None, None
+            
+            if len(new_pop) == len(pop)-1:  # correction when pop has odd number of chromosomes
+                new_pop.append(parent_a)
+
+            else:
+                new_pop.extend([parent_a,parent_b])
+
+        # assigning pop = new_pop here is not going to change pop for the caller, which is not what we intended
+        for i in range(len(new_pop)):
+            pop[i] = new_pop[i]
+
+
+    def __local(self,ch):
+        changed = True
+        while changed:
+            changed = False
+            for i in range(len(ch)):
+                ch[i] = 0 if ch[i] == 1 else 0
+                # TODO
+
+    def __eval_pop(self,pop):
+        # evaluate whole population and sort from fitness high to low
+        pool = Pool(processes=WORKERS)
+        r = pool.starmap(get_fitness, [[ch,i] for i,ch in enumerate(pop) if ch[1] is None])
+        i1,i2 = 0,0
+        while i1<len(r):
+            if pop[i2][1] is None:
+                pop[i2][1] = r[i1]
+                i1 += 1
+            i2 += 1
+        
+        #pop = list(zip(list(zip(*pop))[0],r))
+        # tmp is used to sort None value to the back
+        tmp = min([num for num in pop if num is not None])
+        pop.sort(key=lambda x:x[1] if x[1] is not None else tmp-1, reverse=True)
+
+    def run(self,gen):
+        self.init()
         for _ in range(gen):
-            pass
+            pop_a = self.pop[:int(len(self.pop)*self.p_elite)]
+            pop_b = self.pop[int(len(self.pop)*self.p_elite):]
+            self.__xo(pop_b)
+            self.pop = pop_a + pop_b
+            self.__eval_pop(self.pop)
+            print(self.pop[0][1],self.pop[-1][1])
+
 
 def retr_gcc_flags():
     FLAGS = []
@@ -31,27 +114,56 @@ def retr_gcc_flags():
         FLAGS.extend(f.read().split('\n'))
     return FLAGS[:-1]
 
-
-def get_size(bit_vector,ch_num):
+def get_fitness(ch,ch_num):
+    bit_vector = ch[0]
     cmd = ['gcc']
     for idx,bit in enumerate(bit_vector):
         if bit:
             cmd.append(FLAGS[idx])
     file_list = glob.glob(os.path.join(CBENCH_PATH,TARGET,'src','*.c'))
     cmd.extend(file_list)
-    cmd.extend(['-o',f'{ch_num}'])
-    output = subprocess.check_output(cmd).decode('utf-8')
-    if len(output):
-        print(output)
+    cmd.extend(['-o',f'{TMP_DIR}/{ch_num}'])
+    if not compile(cmd):
+        return None
 
+    cmd = ['size',f'{TMP_DIR}/{ch_num}']
+    size = get_size(cmd)
+
+    return O0_SIZE - size
+
+def compile(cmd):
+    p = subprocess.run(cmd,capture_output=True)
+    # in gcc, warnings and errors are clobbered in stderr for some reason
+    # FIXME : is it possible to seperate them?
+    if p.returncode:    # indicates compile err
+        print(p.returncode,len(p.stdout),len(p.stderr))
+        return False
+    #output = subprocess.check_output(cmd).decode('utf-8')
+    return True
+
+def get_size(cmd):
+    output = subprocess.check_output(cmd).decode('utf-8')
+    # stats = [text,data,bss]
+    stats = [int(i) for i in output.split('\n')[1].split('\t')[:3] if len(i)]
+    return stats[0] + stats[1]
+
+def get_O0_size():
+    cmd = ['gcc','-O0']
+    file_list = glob.glob(os.path.join(CBENCH_PATH,TARGET,'src','*.c'))
+    cmd.extend(file_list)
+    cmd.extend(['-o',f'{TMP_DIR}/O0'])
+    if not compile(cmd):
+        return None
+    cmd = ['size',f'{TMP_DIR}/O0']
+    global O0_SIZE
+    O0_SIZE = get_size(cmd)
 
 def main():
-    #ga = GA()
-    #ga.init()
     global FLAGS
     FLAGS = retr_gcc_flags()
-    pool = Pool(processes=WORKERS)
-    pool.starmap(get_size, [[[0 for _ in range(len(FLAGS))],i] for i in range(WORKERS)] )
+    get_O0_size()
+    ga = GA(len(FLAGS),10)
+    ga.run(100)
 
 
 if __name__=='__main__':
--- util/get_gcc_flags.py
+++ util/get_gcc_flags.py
@@ -1,27 +1,32 @@
 import subprocess
 
-INVALID_CHAR = ['=','.']
+INVALID_CHAR = ['=','.',':']
 
 f = open('../flags','w')
 
 command = ['gcc','--help=optimizers']
 output = subprocess.check_output(command).decode('utf-8')
 prefix = '-f'
-for i in range(len(output)-1):
-    if output[i:i+len(prefix)] == prefix:
+cursor = 0
+while True:
+    if cursor >= len(output)-2:
+        break
+    if output[cursor:cursor+2] == prefix:
         opt = ''
-        current = i
         while True:
-            if output[current] in INVALID_CHAR:
+            if output[cursor] in INVALID_CHAR:
                 break
 
-            elif output[current]==' ':
-                print(opt,file=f)
+            elif output[cursor]==' ':
+                # this flag disables /causes trouble for other flags
+                if opt != '-flive-patching':
+                    print(opt,file=f)
                 #f.write(opt)
                 break
 
             else:
-                opt += output[current]
-                current += 1
+                opt += output[cursor]
+                cursor += 1
+    cursor += 1
 
 f.close()
